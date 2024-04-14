d0cd3183a45a12028d6b1b8eadfda4a9b07479ed
Joseph <josephfodera25@gmail.com>
2024-04-14 14:21:48 -0400

    In class materials

--- courses/itws/inclass/php/PHPSlide2SpaceshipOp.php
+++ courses/itws/inclass/php/PHPSlide2SpaceshipOp.php
@@ -1,7 +1,17 @@
+
+
 <pre>
   <p>Spaceship operator <=>
 
 <?php
+
+/* How it works: 
+
+f the left operand is less than the right operand, <=> returns -1.
+If the left operand is equal to the right operand, <=> returns 0.
+If the left operand is greater than the right operand, <=> returns 1.
+
+*/
 
 // This will only work in PHP7.1+
 $a=1;
@@ -16,6 +26,7 @@ echo "$a<=>$b == ";
 echo $a<=>$b;
 echo '<br/>';
 
+
 $a=2;
 $b=1;
 echo "$a<=>$b == ";
@@ -23,3 +34,4 @@ echo $a<=>$b;
 echo '<br/>';
 ?>
 </pre>
+
