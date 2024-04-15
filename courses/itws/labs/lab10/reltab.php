4436daf998f49292fac23866692b8d6d32e014e2
Joseph <josephfodera25@gmail.com>
2024-04-15 19:25:09 -0400

    Update reltab.php

--- courses/itws/labs/lab10/reltab.php
+++ courses/itws/labs/lab10/reltab.php
@@ -4,3 +4,13 @@
 ?>
 
 <title>PHP &amp; MySQL - ITWS</title>   
+
+<?php
+include('includes/head.inc.php');
+// include global css, javascript, end the head and open the body
+?>
+
+
+<?php include('includes/menubody.inc.php'); ?>
+
+heya
