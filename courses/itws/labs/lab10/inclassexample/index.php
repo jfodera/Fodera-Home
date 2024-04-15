9d3613ff4fd820dbd1f9928d7fdb3ed59610a1fe
Joseph <josephfodera25@gmail.com>
2024-04-15 10:56:37 -0400

    Update index.php

--- courses/itws/labs/lab10/inclassexample/index.php
+++ courses/itws/labs/lab10/inclassexample/index.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
+<html>
+  <head>
 <?php
-include('includes/init.inc.php'); // include the DOCTYPE and opening tags
 include('includes/functions.inc.php'); // functions
 ?>
 <title>PHP &amp; MySQL - ITWS</title>
